3a770d5974227689262696977db54f99c17f8d9b
Derek Smith <derek.smith@intactsoftware.co.uk>
2025-12-17 13:07:27 +0000

    More updates

--- src/AutoMerge/AutoMergeNavigationItem.cs
+++ src/AutoMerge/AutoMergeNavigationItem.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.ComponentModel.Composition;
 using AutoMerge.Base;
 using AutoMerge.VersionControl;
@@ -7,18 +7,18 @@ using Microsoft.VisualStudio.Shell;
 
 namespace AutoMerge
 {
-	[TeamExplorerNavigationItem(GuidList.AutoMergeNavigationItemId, 210, TargetPageId = GuidList.AutoMergePageId)]
-	public class AutoMergeNavigationItem : TeamExplorerNavigationItemBase
-	{
+    [TeamExplorerNavigationItem(GuidList.AutoMergeNavigationItemId, 210, TargetPageId = GuidList.AutoMergePageId)]
+    public class AutoMergeNavigationItem : TeamExplorerNavigationItemBase
+    {
 
-		[ImportingConstructor]
-		public AutoMergeNavigationItem(
-			[Import(typeof(SVsServiceProvider))]
-			IServiceProvider serviceProvider)
-			: base(serviceProvider, GuidList.AutoMergePageId, VersionControlProvider.TeamFoundation)
-		{
-			Text = Resources.AutoMergePageName;
-			Image = Resources.MergeImage;
-		}
-	}
+        [ImportingConstructor]
+        public AutoMergeNavigationItem(
+            [Import(typeof(SVsServiceProvider))]
+            IServiceProvider serviceProvider)
+            : base(serviceProvider, GuidList.AutoMergePageId, VersionControlProvider.TeamFoundation)
+        {
+            Text = Resources.AutoMergePageName;
+            Image = Resources.MergeImage;
+        }
+    }
 }
